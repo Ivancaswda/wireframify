9c31d5262c7d89eddca1726be80df1996fe7db45
aivanius <aivanius@yandex.ru>
2025-11-14 00:20:20 +0700

    teest

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     images:{
         domains: ["ik.imagekit.io"]
-    }
+    },
+    reactStrictMode: false,
+    eslint: {
+        ignoreDuringBuilds: true, // отключает ESLint при сборке
+    },
+    typescript: {
+        ignoreBuildErrors: true, // отключает ошибки TypeScript при сборке
+    },
 };
 
 export default nextConfig;
